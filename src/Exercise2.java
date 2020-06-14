9aac90c13e8bec75686f19cd5d8a5c7722f50709
Pedro Rodriguez <prodriguez@omatech.com>
2020-06-15 01:04:19 +0200

    first exercise

---
+++ src/Exercise2.java
@@ -0,0 +1,16 @@
+
+public class Exercise2 {
+	public static void main(String[] args) {
+        // START FASE 1
+
+        char[] MyName = {'P','e','d','r','o'};
+
+        System.out.println();
+
+        for (char c : MyName) {
+            System.out.println("- " + c);
+        }
+        System.out.println();
+
+    }
+}
--- src/module-info.java
+++
@@ -1,2 +0,0 @@
-module exercici2 {
-}
